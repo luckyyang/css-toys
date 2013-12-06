0fdc2d5ff7697ccffd06ed11b2612b0278fd285c
Liu Jiyang <2411mail@gmail.com>
2013-12-06 09:41:18 +0800

    init

---
+++ rails_modal/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsModal::Application.config.secret_key_base = '722f2e411a52450d6e24f84754859dce35cc21289708b1b87801e818fea13403ffef837684adda8bc25f99196acac46843a43f81263b459c4413e0507538154c'
---
+++ rails_modal/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsModal::Application.config.session_store :encrypted_cookie_store, key: '_rails_modal_session'
